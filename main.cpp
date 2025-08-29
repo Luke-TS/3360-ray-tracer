1ceb4c368d737dc1fd7f8e8f38f804b572c8c0fe
Luke Stewart <132500533+Luke-TS@users.noreply.github.com>
2025-08-29 15:18:56 -0500

    added sphere normals and shading

--- main.cpp
+++ main.cpp
@@ -4,31 +4,45 @@
 
 #include <iostream>
 
-bool hit_shere(const point3& center, double radius, const ray& r) {
+// solve quadratic to determine if ray hits sphere
+// returns smallest positive t value where ray intersects, or -1.0
+// disc < 0 --> no roots (missed)
+// disc = 0 --> single root (ray is tangent)
+// disc > 0 --> 2 roots (entrance and exit intersections)
+double hit_shere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+    if (discriminant >= 0) {
+        return (h - std::sqrt(discriminant)) / a;
+    }
+    else {
+        return -1.0;
+    }
 }
 
 color ray_color(const ray& r) {
-    if( hit_shere(point3(0,0,-1), 0.5, r) )
-        return color(1, 0, 0);
+    auto t = hit_shere(point3(0,0,-1), 0.5, r);
+    if( t > 0.0 ) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1)); // unit surface normal
+
+        // use rgb values as color
+        // adjust values to 0 - 1
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0); // normalize y to 0.0 <= z <= 1.0
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0); // white to blue gradient
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(1.0, 0.0, 0.0); // white to blue gradient
 }
-
-
 
 int main() {
 
     // image
     auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    int image_width = 700;
 
     // calculate correct image height
     int image_height = int(image_width / aspect_ratio);
