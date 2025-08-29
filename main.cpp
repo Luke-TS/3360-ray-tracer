2ab302eaada81f5e8c722df116a2c05220e16ffc
Luke Stewart <132500533+Luke-TS@users.noreply.github.com>
2025-08-29 13:08:59 -0500

    created first ray-traced sphere

--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@
 
 #include <iostream>
 
+bool hit_shere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if( hit_shere(point3(0,0,-1), 0.5, r) )
+        return color(1, 0, 0);
+    
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0); // normalize y to 0.0 <= z <= 1.0
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0); // white to blue gradient
 }
+
+
 
 int main() {
 
